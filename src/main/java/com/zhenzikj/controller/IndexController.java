4b6a1a9d95a3518ffa709504c31ac7d32545756b
Administrator <Administrator@DESKTOP-NLAR89A>
2022-06-25 13:56:18 +0800

    数据库初始化

--- src/main/java/com/zhenzikj/controller/IndexController.java
+++ src/main/java/com/zhenzikj/controller/IndexController.java
@@ -5,22 +5,25 @@ import com.zhenzikj.vo.Blog;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
-@Controller
+@Controller("api/blog")
 public class IndexController {
     @Autowired
     private BlogService blogService;
 
-    @RequestMapping("/index")
-    public String index(Model model){
+    @GetMapping("/{id}")
+    @ResponseBody
+    public Blog getOne(@RequestParam Integer id) {
         List<Blog> blogs = this.blogService.list();
         Blog blog = this.blogService.selectByDbid(2);
-        model.addAttribute("blogs", blogs);
-        model.addAttribute("blog", blog);
-        return "index";
+
+        return blog;
     }
 }
